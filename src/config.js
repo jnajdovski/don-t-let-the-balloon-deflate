32d07e3cd5b02af9834836f6c84dd01d4e9c1e58
jnajdovski <jovan.najdovski@outlook.com>
2022-11-12 03:01:32 +0100

    feat: added physics into config

--- src/config.js
+++ src/config.js
@@ -11,6 +11,19 @@ export default () => {
         height: 1080,
         parent: 'game',
         scene: [new BootScene, new MenuScene, new LoadingScene, new GameScene, new SettingsScene],
+        physics: {
+            default: 'matter',
+            matter: {
+                enableSleeping: true,
+                gravity: {
+                    y: 0
+                },
+                debug: true
+            }
+        },
+        dom: {
+            createContainer: true,                
+        },
         scale: {
             mode: Phaser.Scale.FIT,
             autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
